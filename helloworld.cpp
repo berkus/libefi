e120b480ee2d0f442a603d6120c56c3837fb80c1
Berkus <berkus@gmail.com>
2014-05-28 19:38:43 +0300

    Test protocols requesting.

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,21 +1,33 @@
 // Simple EFI hello-world.
 #include "efi/efi.h"
+#include <wchar.h>
 
 using namespace efi;
 
 extern "C"
 status_t
 EFIAPI
-efi_main(handle_t ImageHandle, system_table_t *st)
+_efi_main(handle_t ImageHandle, system_table_t *st)
 {
-    (void)ImageHandle; // unused
-
-    st->ConOut->output_string(L"\u25ba Welcome\n\r");
-    st->ConOut->output_string(L"Hello EFI World\n\r");
+    st->ConOut->output_string(L"\u25ba Welcome\r\n");
+    st->ConOut->output_string(L"Hello EFI World\r\n");
+    st->ConOut->output_string(L"Firmware vendor: ");
     st->ConOut->output_string(st->FirmwareVendor);
-    st->ConOut->output_string(L"\n\r");
+    st->ConOut->output_string(L"\r\n");
 
-    st->get_runtime().shutdown();
+    auto image = st->get_boot_services()->
+        open_protocol<efi::loaded_image_protocol_t>(ImageHandle, ImageHandle, 0);
+
+    image->Unload(ImageHandle);
+    // wchar_t buf[256];
+    // swprintf(buf, 256, L"%llu", image->ImageSize);
+
+    // st->ConOut->output_string(L"Loaded image size: ");
+    // st->ConOut->output_string(buf);
+    // st->ConOut->output_string(std::to_wstring(image->ImageSize));
+    // st->ConOut->output_string(L"\r\n");
+
+    // st->get_runtime_services().shutdown();
 
     return EFI_SUCCESS;
 }
