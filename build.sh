ccf6699a8aab64957387649cd45d36c660591f8c
Berkus <berkus@gmail.com>
2014-05-28 19:32:28 +0300

    Build all using clang (needs a patch to COFF).

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-/usr/local/gcc-4.8.0-qt-4.8.4-for-mingw32/win32-gcc/bin/i586-mingw32-g++ -std=c++11 -I./include -ffreestanding -Wall -Wextra -fno-stack-protector -fleading-underscore -fshort-wchar -mno-sse -mno-sse2 -mno-mmx -c -o kernel.o helloworld.cpp
-../toolchain/clang/bin/lld -flavor link /out:kernel.efi /entry:efi_main /subsystem:efi_application kernel.o
+CC_CMD="../toolchain/clang/bin/clang++ -target i686-pc--coff -I./include -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/include -std=c++11 -ffreestanding -fno-stack-protector -fpic -fshort-wchar -fno-exceptions -Wall -Wextra -Wno-unused-private-field -Wno-ignored-attributes -mno-sse -mno-sse2 -mno-mmx -c"
+$CC_CMD -o kernel.o helloworld.cpp || exit 1
+$CC_CMD -o constants.o constants.cpp || exit 1
 mkdir -p efiboot/efi/boot
-cp kernel.efi efiboot/
-cp kernel.efi efiboot/efi/boot/bootia32.efi
-
+../toolchain/clang/bin/lld -flavor link /out:efiboot/kernel.efi /entry:efi_main /subsystem:efi_application kernel.o constants.o
+cp efiboot/kernel.efi efiboot/efi/boot/bootia32.efi
