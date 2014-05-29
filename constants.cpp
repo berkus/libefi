a6d25a4ddcc6df6307520e595745bfe2af047e67
Berkus <berkus@gmail.com>
2014-05-29 12:29:19 +0300

    Add loaded_image_device_path_protocol_t with correct guid.

--- constants.cpp
+++ constants.cpp
@@ -3,4 +3,5 @@
 namespace efi {
 constexpr guid_t simple_text_output_protocol_t::guid;
 constexpr guid_t loaded_image_protocol_t::guid;
+constexpr guid_t loaded_image_device_path_protocol_t::guid;
 } // efi namespace
